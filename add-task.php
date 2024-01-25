cdf15715bafe8644731767af5189dc4f0d7edd63
shafaq-here <shafaqisrail@gmail.com>
2024-01-25 19:03:44 +0530

    Add list data in dropdown from MySQL database using php

--- add-task.php
+++ add-task.php
@@ -28,9 +28,68 @@
                 <td>Select List:</td>
                 <td>
                     <select name="list_id" id="">
-                        <option value="1">ToDo</option>
-                        <option value="2">Doing</option>
+
+                        <?php 
+                            //here we will write the code to display the name of lists inside the dropdown
+
+                            //first connect to the database
+                            $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die();
+
+                            //select the database
+                            $db_select = mysqli_select_db($conn,DB_NAME) ;
+
+                            //write the query to select the lists from the tbl_lists
+                            $sql = "SELECT * FROM tbl_lists" ;
+
+                            //execute the sql
+                            $res = mysqli_query($conn,$sql) ;
+
+                            //check of result is successful
+                            if($res==true) {
+                                //we need to know the number of rows to see if there is any data inside this table or not 
+                                $no_of_rows = mysqli_num_rows($res) ;
+
+                                //if the no of rows is >0 then display all the name of the lists and use the list_id value as well
+                                if($no_of_rows > 0) {
+                                    //for each row inside the table print it using the dropdown, you might need to end and open new php for that
+                                    while($row=mysqli_fetch_assoc($res)) {
+                                        $list_id = $row['list_id'] ;
+                                        $list_name = $row['list_name'] ;
+
+                                        ?>
+                                        <option value="<?php echo $list_id ?>"><?php echo $list_name?></option>
+                                        
+                                        <?php
+                                    }
+                                }
+                                else {
+                                    //no rows hence no list found
+                                    ?>
+                                    <option value="0">None</option>
+
+                                    <?php
+                                }
+                            }
+                        ?>
+                        
+                        
                     </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Priority:</td>
+                <td>
+                    <select name="priority" id="">
+                        <option value="High">High</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Low">Low</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Deadline:</td>
+                <td>
+                    <input type="date" name="deadline">
                 </td>
             </tr>
         </table>
