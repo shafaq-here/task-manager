5751a9cde788a6605df52743b1d488fc6f1b5c5d
shafaq-here <shafaqisrail@gmail.com>
2024-01-26 15:34:28 +0530

    Insert new task into database using form data

--- add-task.php
+++ add-task.php
@@ -10,6 +10,14 @@
 <body>
     <h1>TASK MANAGER</h1>
     <a href="<?php echo SITEURL?>">Home</a>
+
+    <p>
+        <?php
+        if(isset($_SESSION['add-task-failed'])) {
+            echo $_SESSION['add-task-failed'] ;
+        } 
+        ?>
+    </p>
     <h3>Welcome to Add Task page</h3>
 
     <form method="POST" action="">
@@ -27,7 +35,7 @@
             <tr>
                 <td>Select List:</td>
                 <td>
-                    <select name="list_id" id="">
+                    <select name="list_id" >
 
                         <?php 
                             //here we will write the code to display the name of lists inside the dropdown
@@ -92,7 +100,55 @@
                     <input type="date" name="deadline">
                 </td>
             </tr>
+            <tr>
+                <td>
+                    <input type="submit" value="Save" name="submit">
+                </td>
+            </tr>
         </table>
     </form>
 </body>
 </html>
+
+<?php 
+    //check if the form was submitted
+    if(isset($_POST['submit'])) {
+        //echo $_POST['priority'] ;
+        $task_name = $_POST['task_name'] ;
+        $task_description = $_POST['task_description'] ;
+        $list_id = $_POST['list_id'] ;
+        $priority = $_POST['priority'] ;
+        $deadline = $_POST['deadline'] ;
+
+        //we have the data from the form now we will insert the data into the database
+
+        //connedt to the database
+        $conn2 = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die() ;
+
+        $db_select2 = mysqli_select_db($conn2,DB_NAME);
+
+        //write the sql query to insert the data into the task table
+        $sql2 = "INSERT INTO tbl_tasks SET
+        task_name = '$task_name',
+        task_description = '$task_description',
+        list_id = $list_id,
+        priority = '$priority',
+        deadline = '$deadline'
+        " ;
+
+        //execute the sql query
+        $res2 = mysqli_execute_query($conn2,$sql2) ;
+
+        if($res2==true) {
+            //query was successful so set a session message for successful addition of task and redirect to home
+            $_SESSION['add-task-success'] = "Task was successfully added" ;
+
+            header('location:'.SITEURL) ;
+        }
+        else {
+            //failed to add the task for some reason so redirect back to add-task page to try again
+            $_SESSION['add-task-failed'] = "Failed to add task, Try Again!";
+            header('location:'.SITEURL.'add-task.php') ;
+        }
+    }
+?>
